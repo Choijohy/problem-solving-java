bc67998988ded2b8d6392d8e22737b03e8192633
choijohy <gitjeehee7948@git.vhost>
2025-01-19 14:47:32 +0900

    1/19

---
+++ src/main/java/array/GetRanking.java
@@ -0,0 +1,30 @@
+package array;
+
+import java.util.*;
+
+public class GetRanking {
+    public static int[] solution(int num, int[] scores){
+        int[] result = new int[num];
+        for (int i=0; i<num; i++){
+            int count = 0;
+            int current = scores[i];
+            for (int j=0; j<num;j++){
+                if (scores[j] > current) count ++;
+            }
+            result[i] = count+1;
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int num = in.nextInt();
+        int[] scores = new int[num];
+        for (int i=0; i<num; i++){
+            scores[i] = in.nextInt();
+        }
+        int[] result = solution(num, scores);
+        for (int i=0; i< result.length;i++){
+            System.out.print(result[i]+" ");
+        }
+    }
+}

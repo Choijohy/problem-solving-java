4baff2c8a78bd1b354905b29701aa7768a027ea0
choijohy <gitjeehee7948@git.vhost>
2025-03-14 14:01:22 +0900

    .

--- src/main/java/정렬/PhoneBook.java
+++ src/main/java/정렬/PhoneBook.java
@@ -1,8 +1,14 @@
 package 정렬;
- import java.util.Arrays;
+/*
+문제: 전화번호 접두사 인증
+유형: 문자열 정렬
+    - 숫자를 정렬할때는 아스키 번호 기준
+    - 0001 -> 001 -> 01
+*/
 
- class PhoneBook {
+import java.util.Arrays;
 
+class PhoneBook {
      public boolean solution(String[] phone_book) {
          Arrays.sort(phone_book);
          String prev = phone_book[0];
@@ -12,4 +18,5 @@ package 정렬;
          }
          return true;
      }
- }
+}
+
